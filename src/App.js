85db6d109826c765687f307bc3f4fc4f9a1486df
HarunHatiboglu <harun.hatiboglu@helloworld.com.tr>
2023-08-25 09:39:17 +0300

    Add commit Context

--- src/App.js
+++ src/App.js
@@ -4,43 +4,45 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Products from './pages/Products';
 import Detail from './pages/Detail';
 import Login from './pages/Login';
-
+import AuthProvider from './context/AuthProvider/AuthProvider';
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="loginPage"
-          component={Login}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="ProductsPage"
-          component={Products}
-          options={{
-            title: 'Mağazam',
-            headerStyle: {backgroundColor: '#2f4f4f'},
-            headerTitleStyle: {color: '#fff', fontSize: 20},
-            headerTitleAlign: 'center',
-          }}
-        />
-        <Stack.Screen
-          name="DetailPage"
-          component={Detail}
-          options={{
-            title: 'Ürün Detay',
-            headerStyle: {backgroundColor: '#2f4f4f'},
-            headerTitleStyle: {color: '#fff', fontSize: 20},
-            headerTitleAlign: 'center',
-            headerTintColor: '#fff',
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="loginPage"
+            component={Login}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="ProductsPage"
+            component={Products}
+            options={{
+              title: 'Mağazam',
+              headerStyle: {backgroundColor: '#2f4f4f'},
+              headerTitleStyle: {color: '#fff', fontSize: 20},
+              headerTitleAlign: 'center',
+            }}
+          />
+          <Stack.Screen
+            name="DetailPage"
+            component={Detail}
+            options={{
+              title: 'Ürün Detay',
+              headerStyle: {backgroundColor: '#2f4f4f'},
+              headerTitleStyle: {color: '#fff', fontSize: 20},
+              headerTitleAlign: 'center',
+              headerTintColor: '#fff',
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthProvider>
   );
 };
 
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,11 +1,13 @@
-import {View, SafeAreaView, Alert} from 'react-native';
-import {API_AUTH_URL} from '@env';
 import React from 'react';
+import {View, SafeAreaView, Alert} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {Formik} from 'formik';
+import {API_AUTH_URL} from '@env';
+
 import styles from './Login.style';
 import Input from '../../components/Input/Input';
 import Button from '../../components/Button/Button';
 import LottieView from 'lottie-react-native';
-import {Formik} from 'formik';
 
 import usePost from '../../hooks/usePost/usePost';
 
@@ -16,12 +18,13 @@ const Login = ({navigation}) => {
     post(API_AUTH_URL, values);
   }
   if (error) {
-    Alert.alert('Shopping', 'Bir hata oluştu');
+    Alert.alert('Magazada', 'Bir hata oluştu');
   }
   if (data) {
     if (data.status === 'Error') {
-      Alert.alert('Shopping', 'Kullanıcı Bulunamadı');
+      Alert.alert('Magazada', 'Kullanıcı Bulunamadı');
     } else {
+      AsyncStorage.setItem('@USER', JSON.stringify(user));
       navigation.navigate('ProductsPage');
     }
   }
@@ -66,3 +69,26 @@ const Login = ({navigation}) => {
 };
 
 export default Login;
+
+const user = {
+  address: {
+    geolocation: {
+      lat: '-37.3159',
+      long: '81.1496',
+    },
+    city: 'kilcoole',
+    street: 'new road',
+    number: 7682,
+    zipcode: '12926-3874',
+  },
+  id: 1,
+  email: 'john@gmail.com',
+  username: 'johnd',
+  password: 'm38rmF$',
+  name: {
+    firstname: 'john',
+    lastname: 'doe',
+  },
+  phone: '1-570-236-7033',
+  __v: 0,
+};
